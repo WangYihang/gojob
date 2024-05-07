fcb0192f11c11c158ef61b2a98ab18fa20c75a8c
Yihang Wang <wangyihanger@gmail.com>
2024-05-07 14:23:02 +0800

    feat: add default labels by NewRegistryWithLabels

--- go.mod
+++ go.mod
@@ -6,12 +6,12 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/prometheus/client_golang v1.19.0
+	github.com/prometheus/client_model v0.6.1
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
--- prometheus.go
+++ prometheus.go
@@ -11,69 +11,94 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/push"
+	io_prometheus_client "github.com/prometheus/client_model/go"
 )
 
 var (
-	numTotal = prometheus.NewGaugeVec(
+	numTotal = prometheus.NewGauge(
 		prometheus.GaugeOpts{
 			Name: "num_total",
 			Help: "Total number of processed events",
 		},
-		[]string{"version", "runner_ip", "runner_country", "runner_region", "runner_city"},
 	)
-	numFailed = prometheus.NewGaugeVec(
+	numFailed = prometheus.NewGauge(
 		prometheus.GaugeOpts{
 			Name: "num_failed",
 			Help: "Total number of failed events",
 		},
-		[]string{"version", "runner_ip", "runner_country", "runner_region", "runner_city"},
 	)
-	numSucceed = prometheus.NewGaugeVec(
+	numSucceed = prometheus.NewGauge(
 		prometheus.GaugeOpts{
 			Name: "num_succeed",
 			Help: "Total number of succeeded events",
 		},
-		[]string{"version", "runner_ip", "runner_country", "runner_region", "runner_city"},
 	)
-	numFinished = prometheus.NewGaugeVec(
+	numFinished = prometheus.NewGauge(
 		prometheus.GaugeOpts{
 			Name: "num_finished",
 			Help: "Total number of finished events",
 		},
-		[]string{"version", "runner_ip", "runner_country", "runner_region", "runner_city"},
 	)
 )
 
+type customMetricsRegistry struct {
+	*prometheus.Registry
+	customLabels []*io_prometheus_client.LabelPair
+}
+
+func NewRegistryWithLabels(labels map[string]string) *customMetricsRegistry {
+	c := &customMetricsRegistry{
+		Registry: prometheus.NewRegistry(),
+	}
+	for k, v := range labels {
+		c.customLabels = append(c.customLabels, &io_prometheus_client.LabelPair{
+			Name:  &k,
+			Value: &v,
+		})
+	}
+	return c
+}
+
+func (g *customMetricsRegistry) Gather() ([]*io_prometheus_client.MetricFamily, error) {
+	metricFamilies, err := g.Registry.Gather()
+
+	for _, metricFamily := range metricFamilies {
+		metrics := metricFamily.Metric
+		for _, metric := range metrics {
+			metric.Label = append(metric.Label, g.customLabels...)
+		}
+	}
+
+	return metricFamilies, err
+}
 func prometheusPusher(url, job string, statusChan <-chan Status, wg *sync.WaitGroup) {
-	registry := prometheus.NewRegistry()
-	registry.MustRegister(numTotal, numFailed, numSucceed, numFinished)
-	registry.MustRegister(
-		collectors.NewGoCollector(),
-		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
-	)
 	instance := fmt.Sprintf(
 		"gojob-%s-%s-%s",
 		version.Version,
 		strings.ToLower(runner.Runner.Country),
 		runner.Runner.IP,
 	)
-	labels := prometheus.Labels{
+	registry := NewRegistryWithLabels(map[string]string{
 		"version":        version.Version,
 		"runner_ip":      runner.Runner.IP,
 		"runner_country": runner.Runner.Country,
 		"runner_region":  runner.Runner.Region,
 		"runner_city":    runner.Runner.City,
-	}
+		"instance":       instance,
+	})
+	registry.MustRegister(numTotal, numFailed, numSucceed, numFinished)
+	registry.MustRegister(
+		collectors.NewGoCollector(),
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+	)
 	go func() {
 		for status := range statusChan {
 			slog.Info("promehteus pusher", slog.Any("status", status))
-			numTotal.With(labels).Set(float64(status.NumTotal))
-			numFailed.With(labels).Set(float64(status.NumFailed))
-			numSucceed.With(labels).Set(float64(status.NumSucceed))
-			numFinished.With(labels).Set(float64(status.NumFinished))
-			if err := push.New(url, job).Grouping(
-				"instance", instance,
-			).Gatherer(registry).Push(); err != nil {
+			numTotal.Set(float64(status.NumTotal))
+			numFailed.Set(float64(status.NumFailed))
+			numSucceed.Set(float64(status.NumSucceed))
+			numFinished.Set(float64(status.NumFinished))
+			if err := push.New(url, job).Gatherer(registry).Push(); err != nil {
 				slog.Error("error occurred while pushing to prometheus", slog.String("error", err.Error()))
 			}
 		}
