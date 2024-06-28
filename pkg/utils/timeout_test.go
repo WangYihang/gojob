f34ef7b06eb521f4dff7285faaa847682b04038a
Yihang Wang <wangyihanger@gmail.com>
2024-06-28 16:10:34 +0800

    feat: Add timeout context to Task struct and refactor RunWithTimeout test

--- pkg/utils/timeout_test.go
+++ pkg/utils/timeout_test.go
@@ -1,19 +1,25 @@
 package utils_test
 
 import (
+	"context"
 	"testing"
 	"time"
 
 	"github.com/WangYihang/gojob/pkg/utils"
 )
 
-func TestRunWithTimeout(t *testing.T) {
-	task := func() error {
-		time.Sleep(2 * time.Second)
-		return nil
-	}
+type Task struct {
+	ctx context.Context
+}
 
-	err := utils.RunWithTimeout(task, 1*time.Second)
+func (t *Task) Do() error {
+	time.Sleep(16 * time.Second)
+	return nil
+}
+
+func TestRunWithTimeout(t *testing.T) {
+	task := &Task{context.Background()}
+	err := utils.RunWithTimeout(task.Do, 1*time.Second)
 	if err == nil {
 		t.Errorf("Expected timeout error, got nil")
 	}
